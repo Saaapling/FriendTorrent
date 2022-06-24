7f95f7bc4daa39a32cb1dcbcf8369e61fc61a625
deepdreamr <dreamer.kelemen@gmail.com>
2022-06-23 22:04:05 -0400

    Update git ignore + fixed project target.

---
+++ BTProtocol/BTProtocol/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTProtocol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test display");
+            Console.ReadLine();
+        }
+    }
+}
